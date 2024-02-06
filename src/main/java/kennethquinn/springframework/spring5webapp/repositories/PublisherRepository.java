42f5252a7b4d12fc0093e18c59350f7f8cce4176
Kenneth Quinn <kqui108@wgu.edu>
2024-02-05 18:52:57 -0600

    Add new PublisherRepository interface
    The PublisherRepository interface has been created, extending CrudRepository for the Publisher domain. This will allow basic CRUD operations for the Publisher entity in the application.

---
+++ src/main/java/kennethquinn/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,11 @@
+package kennethquinn.springframework.spring5webapp.repositories;
+
+import kennethquinn.springframework.spring5webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Kenneth Quinn 2/5/2024
+ */
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
