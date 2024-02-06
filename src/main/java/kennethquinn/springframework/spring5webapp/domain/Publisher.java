8b1499f16d3c0348af3e5d48283c99b9631ae2b0
Kenneth Quinn <kqui108@wgu.edu>
2024-02-05 18:50:45 -0600

    Add new Publisher Entity
    Created a new Publisher Entity with fields for address, city, state, zip code and a set of Publishers. This Entity provides a representation for publishers of books in our application, allowing us to store and retrieve publisher-specific data.

---
+++ src/main/java/kennethquinn/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,107 @@
+package kennethquinn.springframework.spring5webapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Created by Kenneth Quinn on 2/5/2024
+ */
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    @OneToMany
+    private Set<Publisher> publisher;
+
+    public Publisher() {
+    }
+
+    public Publisher(String address, String city, String state, String zip) {
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Set<Publisher> getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Set<Publisher> publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", publisher=" + publisher +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
